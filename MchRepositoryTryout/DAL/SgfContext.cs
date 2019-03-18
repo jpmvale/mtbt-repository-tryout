f4899e56f129ac64c9191761fd8ada8341aea1a0
Joao Pedro Vale <jpmvale@gmail.com>
2019-03-18 12:56:51 -0300

    Creating connection with database

--- MchRepositoryTryout/DAL/SgfContext.cs
+++ MchRepositoryTryout/DAL/SgfContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using MchRepositoryTryout.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MchRepositoryTryout.DAL
 {
-    public class SgfContext : DbContext;
+    public class SgfContext : DbContext
     {
+        public SgfContext() : base("name=SgfContext")
+        {
+             
+        }
+
+        public virtual IDbSet<Segments> Segments { get; set; }
     }
 }
---
+++ MchRepositoryTryout/Models/Segments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MchRepositoryTryout.Models
+{
+    [Table("tbsegments")]
+    public class Segments 
+    {
+        [Column("location")]
+        public int Location { get; set; }
+        [Column("segment")]
+        public string Segment { get; set; }
+    }
+}
